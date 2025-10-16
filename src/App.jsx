4440e1160c432a2f40bec5a8c45a0f6ead238161
Twilight Goblin <ayushmanjunathas@gmail.com>
2025-10-16 07:55:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+"use client";
+import React from "react";
+import { motion } from "motion/react";
+import { LampContainer } from "@/components/ui/lamp"; // use @ alias if set up
+import SplashCursor from './components/SplashCursor'
+
+export function LampDemo() {
+  return (
+    <LampContainer>
+      <motion.h1
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+      >
+        Build lamps <br /> the right way
+      </motion.h1>
+    </LampContainer>
+  );
+}
+
+const App = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black">
+      <LampDemo />
+      
+
+<SplashCursor />
+    </div>
+  );
+};
+
+export default App;
